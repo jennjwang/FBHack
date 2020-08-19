f5a6509e66c3a26d75175269fb34e47c8eec2b39
rachWei <rachaelwei@gmail.com>
2020-08-18 22:48:21 -0400

    added chatbot page

--- wit.ai/ContentView.swift
+++ wit.ai/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        login()
+        //seeing if they appear
+        VStack{
+            login()
+            chatbot()
+        }
     }
 }
 
---
+++ wit.ai/chatbot.swift
@@ -0,0 +1,21 @@
+//
+//  chatbot.swift
+//  wit.ai
+//
+//  Created by Rachael Wei on 8/18/20.
+//  Copyright © 2020 Rachael. All rights reserved.
+//
+
+import SwiftUI
+
+struct chatbot: View {
+    var body: some View {
+        Text("chatbot page")
+    }
+}
+
+struct chatbot_Previews: PreviewProvider {
+    static var previews: some View {
+        chatbot()
+    }
+}
