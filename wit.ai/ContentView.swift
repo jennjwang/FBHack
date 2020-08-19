7ac1eb0a8fb7d12e34362a5a392244650e024301
rachWei <rachaelwei@gmail.com>
2020-08-18 22:38:53 -0400

    Initial Commit

---
+++ wit.ai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wit.ai
+//
+//  Created by Rachael Wei on 8/18/20.
+//  Copyright © 2020 Rachael. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
